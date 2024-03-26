d7b14d2ceb5e58a9e1254efd5fff956796d4dd34
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-03-26 08:29:36 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Jackson Harrison on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
